5f0480d56818730cb57698724c432116d2d41fff
Dat <phungdat020501@gmail.com>
2021-12-09 19:02:49 +0700

    create repo

---
+++ Asm2AppDev/Asm2AppDev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asm2AppDev.Startup))]
+namespace Asm2AppDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
